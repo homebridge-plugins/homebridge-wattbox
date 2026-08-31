fca7fedf231d9b7c2bfad25fa1c4182109124ebf
Derek Miller <1340081+derek-miller@users.noreply.github.com>
2026-08-31 14:25:25 -0500

    use scoped package name for PLUGIN_NAME

--- src/settings.ts
+++ src/settings.ts
@@ -7,4 +7,4 @@ export const PLATFORM_NAME = 'WattBox';
 /**
  * This must match the name of your plugin as defined the package.json
  */
-export const PLUGIN_NAME = 'homebridge-wattbox';
+export const PLUGIN_NAME = '@homebridge-plugins/homebridge-wattbox';
